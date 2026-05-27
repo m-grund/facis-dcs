f67c2c1a6f60722087b40af369146b50817a2b0b
Marvin Grund <m.grund@neusta.de>
2026-05-27 14:48:47 +0200

    Frontend: - add linter and formatter configuration - add pre-commit hook and workflow

---
+++ frontend/ClientApp/eslint.config.js
@@ -0,0 +1,33 @@
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import skipFormattingConfig from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+  {
+    ignores: ['node_modules/**', 'dist/**', '.vite/**'],
+  },
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommendedTypeChecked,
+  vueTsConfigs.stylisticTypeChecked,
+  {
+    files: ['**/*.vue', '**/*.ts'],
+    rules: {
+      'vue/require-default-prop': 'off',
+      '@typescript-eslint/use-unknown-in-catch-callback-variable': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      'vue/no-unused-vars': [
+        'error',
+        {
+          ignorePattern: '^_',
+        },
+      ],
+    },
+  },
+  skipFormattingConfig,
+)
