7e4157332d05d293048e4e140a76e9fe4cc222a8
Daniel Simon <d.simon@neusta.de>
2026-06-09 11:17:02 +0200

    Add state to contract detail view

--- frontend/ClientApp/src/modules/contract-workflow-engine/components/ContractDetailsEditor.vue
+++ frontend/ClientApp/src/modules/contract-workflow-engine/components/ContractDetailsEditor.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="card border border-base-300 bg-base-100 shadow-sm">
     <div class="card-body gap-5">
+      
+      <h2 class="card-title justify-between text-sm">
+        <div class="flex gap-2">
+          Contract Details
+        </div>
+        <div class="badge badge-sm badge-secondary">{{ contract.state }}</div>
+      </h2>
+
       <fieldset class="fieldset border-none p-0">
         <legend class="fieldset-legend">Version: {{ contract.contract_version }}</legend>
       </fieldset>
