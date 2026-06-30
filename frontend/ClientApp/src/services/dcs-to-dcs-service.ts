4dd083cf3aacb6c851a88c2d777b61625630d1f0
Daniel Simon <d.simon@neusta.de>
2026-06-30 09:35:20 +0200

    Add service to call sync endpoint

---
+++ frontend/ClientApp/src/services/dcs-to-dcs-service.ts
@@ -0,0 +1,9 @@
+import http from '@/api/http'
+
+export interface ContractSyncRequestResponse {
+    from_peer_did: string
+}
+
+export async function requestContractSync(did: string): Promise<ContractSyncRequestResponse> {
+  return http.get<ContractSyncRequestResponse>('/peer/contracts/sync', { params: { did } }).then((res) => res.data)
+}
